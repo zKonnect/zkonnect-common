e3788d32ec2ad18dbff382bb1d680a52e61fdc4c
Debopom-Banerjee <debopombanerjee752@gmail.com>
2024-10-06 03:37:21 +0530

    feat: creator-dashboard

--- src/app/creator-dashboard/event/[eventId]/page.tsx
+++ src/app/creator-dashboard/event/[eventId]/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
-import { Copy, CalendarIcon, CircleCheck } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { CircleCheck, MoveRight } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Copy, Check } from "lucide-react";
+import Image from "next/image";
+import { CalendarIcon, LinkIcon, EyeIcon } from "lucide-react";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
 interface EventPageProps {
   params: {
@@ -23,74 +28,175 @@ interface EventPageProps {
 }
 
 export default function EventPage({ params }: EventPageProps) {
+  const [copiedBlink, setCopiedBlink] = useState<boolean>(false);
+  const [copiedMeet, setCopiedMeet] = useState<boolean>(false);
+  const [blinkUrl, setBlinkUrl] = useState<string>(
+    "https://zkonnect.blinks.com/event",
+  );
+  const [meetUrl, setMeetUrl] = useState<string>(
+    "https://zkonnect.meet.com/id",
+  );
+
+  const onCopy = (url: string, setCopied: (value: boolean) => void) => {
+    navigator.clipboard.writeText(url);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
   return (
     <div className="container mx-auto mt-8 p-4">
       <Card className="w-full">
         <CardHeader className="bg-[#FF6D4D] text-white">
           <CardTitle className="text-md">Event Details</CardTitle>
           <Separator />
-          <h2 className="mt-4 text-4xl font-bold">{params.title}</h2>
+          <h2 className="mt-4 text-4xl font-bold">Event title</h2>
+          {/* <h2 className="mt-4 text-4xl font-bold">{params.title}</h2> */}
           <div className="mt-2 flex items-center">
             <CalendarIcon className="mr-2" />
-            <span>Starts on {params.startTime}</span>
+            <span>Starts on 12:00 a.m.</span>
+            {/* <span>Starts on {params.startTime}</span> */}
           </div>
         </CardHeader>
         <CardContent className="p-6">
           <div className="flex justify-between">
             <div className="w-2/3">
               <h3 className="mb-4 text-2xl font-bold">Description</h3>
-              <p className="mb-8">{params.desc}</p>
+              <p className="mb-8">Event description.</p>
+              {/* <p className="mb-8">{params.desc}</p> */}
 
-              <h3 className="mb-2 text-xl font-bold">Ticket Details</h3>
-              <p className="mb-2 text-gray-600">
-                Here&apos;s the detailed listing:
-              </p>
-              <div className="mb-8 grid grid-cols-3 gap-4">
-                <div>
-                  <p className="font-bold">Price</p>
-                  <p>{params.price}</p>
+              <div className="mb-4 border p-4">
+                <div className="mb-4 flex items-center">
+                  <Image
+                    src="/assets/dashboard/ticket-icon.svg"
+                    width={20}
+                    height={20}
+                    alt="ticket-icon"
+                    className="mr-4"
+                  />
+                  <h5 className="font-bold">Ticket Details</h5>
                 </div>
-                <div>
-                  <p className="font-bold">Total ticket</p>
-                  <p>{params.totalTickets}</p>
-                </div>
-                <div>
-                  <p className="font-bold">Token Type</p>
-                  <p>{params.tokenType}</p>
+                <p className="mb-4 text-gray-600">
+                  Here's the detailed listing:
+                </p>
+                <div className="grid grid-cols-3 gap-4">
+                  <div className="rounded-lg bg-[#F8F8F8] p-3">
+                    <div className="flex items-center justify-between">
+                      <p className="font-bold">Price</p>
+                      <p>200</p>
+                      {/* <p>{params.price}200</p> */}
+                    </div>
+                  </div>
+                  <div className="rounded-lg bg-[#F8F8F8] p-3">
+                    <div className="flex items-center justify-between">
+                      <p className="font-bold">Total ticket</p>
+                      <p> 50</p>
+                      {/* <p>{params.totalTickets} 50</p> */}
+                    </div>
+                  </div>
+                  <div className="rounded-lg bg-[#F8F8F8] p-3">
+                    <div className="flex items-center justify-between">
+                      <p className="font-bold">Token Type</p>
+                      <div className="flex items-center space-x-2">
+                        <Avatar className="size-7">
+                          <AvatarImage src="https://assets.coingecko.com/coins/images/4128/standard/solana.png?1718769756" />
+                          <AvatarFallback>SOL</AvatarFallback>
+                        </Avatar>
+                        <span>SOL</span>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
 
-              <h3 className="mb-2 text-xl font-bold">Meeting Link</h3>
-              <div className="mb-4 flex items-center">
-                <Input value={params.meetingLink} readOnly />
-                <Button variant="outline" size="icon" className="ml-2">
-                  <Copy className="h-4 w-4" />
-                </Button>
-              </div>
-
-              <h3 className="mb-2 text-xl font-bold">Blink</h3>
-              <div className="flex items-center">
-                <Input value={params.blinkLink} readOnly />
-                <Button variant="outline" size="icon" className="ml-2">
-                  <Copy className="h-4 w-4" />
-                </Button>
+              <div className="w-1/2 gap-2 border p-2">
+                <div className="mb-2 flex items-center space-x-2">
+                  <div className="grid flex-1 gap-2">
+                    <Label
+                      htmlFor="meet-link"
+                      className="mb-1 flex items-center"
+                    >
+                      <Image
+                        src="/assets/meet-icon.svg"
+                        width={20}
+                        height={20}
+                        alt="meet-icon"
+                        className="mr-2"
+                      />
+                      <span>Meeting Link</span>
+                    </Label>
+                    <div className="flex gap-2">
+                      <Input
+                        id="meet-link"
+                        value={meetUrl}
+                        readOnly
+                        onClick={() => onCopy(meetUrl, setCopiedMeet)}
+                      />
+                      <Button
+                        type="submit"
+                        size="sm"
+                        className="h-full bg-[#808080] px-3"
+                        onClick={() => onCopy(meetUrl, setCopiedMeet)}
+                        disabled={copiedMeet}
+                      >
+                        <span className="sr-only">Copy</span>
+                        {copiedMeet ? <Check size={20} /> : <Copy size={20} />}
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="mb-2 flex items-center space-x-2">
+                  <div className="grid flex-1 gap-2">
+                    <Label
+                      htmlFor="blink-link"
+                      className="mb-1 flex items-center"
+                    >
+                      <Image
+                        src="/assets/blink-icon.svg"
+                        width={20}
+                        height={20}
+                        alt="blink-icon"
+                        className="mr-2"
+                      />
+                      <span>Blink</span>
+                    </Label>
+                    <div className="flex gap-2">
+                      <Input
+                        id="blink-link"
+                        value={blinkUrl}
+                        readOnly
+                        onClick={() => onCopy(blinkUrl, setCopiedBlink)}
+                      />
+                      <Button
+                        type="submit"
+                        size="sm"
+                        className="h-full bg-[#808080] px-3"
+                        onClick={() => onCopy(blinkUrl, setCopiedBlink)}
+                        disabled={copiedBlink}
+                      >
+                        <span className="sr-only">Copy</span>
+                        {copiedBlink ? <Check size={20} /> : <Copy size={20} />}
+                      </Button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
 
-            <div className="w-1/3">
+            <div className="mx-auto w-1/4 border p-3">
               <h3 className="mb-4 text-2xl font-bold">Event Banner</h3>
               <div className="w-54 h-64 rounded-lg bg-[#FF6D4D]"></div>
             </div>
           </div>
+          <div className="mt-4 flex justify-end">
+            <Button className="space-x-6 bg-black px-6 py-6 text-sm">
+              <CircleCheck size={20} />
+              <span>Cancel Event</span>
+            </Button>
+          </div>
         </CardContent>
       </Card>
-
-      <div className="mt-4 flex justify-end">
-        <Button className="space-x-6 bg-black px-6 py-6 text-sm">
-          <CircleCheck size={20} />
-          <span>Cancel Event</span>
-        </Button>
-      </div>
     </div>
   );
 }
--- src/app/creator-dashboard/page.tsx
+++ src/app/creator-dashboard/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 
 import { MoveRight } from "lucide-react";
+import Image from "next/image";
 
 import { constructMetaData } from "@/lib/metadata";
 import { Button } from "@/components/ui/button";
 import Events from "./_components/events";
+import EventPage from "./event/[eventId]/page";
 import Charts from "./_components/charts";
 import Profile from "./_components/profile";
 import CreateEvent from "./_components/create-event";
@@ -14,11 +16,107 @@ export const metadata = constructMetaData({
   description: "Creator Dashboard of zKonnect",
 });
 
-const CreatorDashboardPage = () => {
+const CreatorDashboard = () => {
+  // return (
+  //   <section className="mx-10 max-w-[1600px] px-6 py-6 2xl:px-2">
+  //     <div className="mb-6 flex items-center justify-between pt-18">
+  //       <div>
+  //         <h1 className="text-xl font-bold">Hey, Michael Angelio</h1>
+  //         <p className="text-gray-500">Creator</p>
+  //       </div>
+
+  //       <div className="flex flex-row gap-4">
+  //         <div className="flex items-center rounded border border-gray-200 p-2">
+  //           <Image
+  //             src="/assets/dashboard/total-event-icon.svg"
+  //             width={20}
+  //             height={20}
+  //             alt="total-event-icon"
+  //             className="mr-2"
+  //           />
+  //           <p className="mr-3 text-sm text-gray-500">Total Events</p>
+  //           <p className="mr-2 font-bold">50</p>
+  //         </div>
+  //         <div className="flex items-center rounded border border-gray-200 p-2">
+  //           <Image
+  //             src="/assets/dashboard/earning-icon.svg"
+  //             width={20}
+  //             height={20}
+  //             alt="earning-icon"
+  //             className="mr-2"
+  //           />
+  //           <p className="mr-3 text-sm text-gray-500">Earning</p>
+  //           <p className="font-bold">$8,143,429</p>
+  //         </div>
+  //       </div>
+  //     </div>
+
+  //     <div className="rounded-md border p-4">
+  //       <div className="mb-4 flex items-center justify-between">
+  //         <h2 className="text-xl font-semibold">My Events</h2>
+  //         <Link href="/create-event">
+  //           <Button className="space-x-6 bg-black px-5 py-5 text-sm">
+  //             <span>Create event</span>
+  //             <MoveRight size={20} />
+  //           </Button>
+  //         </Link>
+  //       </div>
+
+  //       <div className="flex gap-4">
+  //         <div className="w-[25%]">
+  //           <Events />
+  //         </div>
+  //         <div className="w-[75%]">
+  //           <EventPage />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </section>
+  // );
+
   return (
-    <section className="mx-auto max-w-[1600px] px-6 py-6 2xl:px-2">
-      <div className="mb-6 flex items-center justify-between pt-24">
-        <h1 className="text-2xl font-bold text-black lg:text-3xl">Dashboard</h1>
+    <section className="mx-10 max-w-[1600px] px-6 py-6 2xl:px-2">
+      <div className="mb-6 flex items-center justify-between pt-18">
+        <div>
+          <h1 className="text-xl font-bold">Hey, Michael Angelio</h1>
+          <p className="text-gray-500">Creator</p>
+        </div>
+
+        <div className="flex flex-row gap-4">
+          <div className="flex items-center rounded border border-gray-200 p-2">
+            <Image
+              src="/assets/dashboard/total-event-icon.svg"
+              width={20}
+              height={20}
+              alt="total-event-icon"
+              className="mr-2"
+            />
+            <p className="mr-3 text-sm text-gray-500">Total Events</p>
+            <p className="mr-2 font-bold">0</p>
+          </div>
+          <div className="flex items-center rounded border border-gray-200 p-2">
+            <Image
+              src="/assets/dashboard/earning-icon.svg"
+              width={20}
+              height={20}
+              alt="earning-icon"
+              className="mr-2"
+            />
+            <p className="mr-3 text-sm text-gray-500">Earning</p>
+            <p className="font-bold">$0.00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex h-[calc(100vh-200px)] flex-col items-center justify-center rounded-md border p-4">
+        <p className="mb-4 text-center text-xl font-semibold text-[#808080]">
+          Create an Event and Deliver Unforgettable <br />
+          Experiences in Real-Time! 😮‍💨
+        </p>
+        <p className="mb-4 text-center text-sm font-normal text-[#808080]">
+          Looks like you haven't created one yet—start now to <br />
+          deliver unforgettable real-time experiences!
+        </p>
         <Link href="/create-event">
           <Button className="space-x-6 bg-black px-5 py-5 text-sm">
             <span>Create event</span>
@@ -26,22 +124,8 @@ const CreatorDashboardPage = () => {
           </Button>
         </Link>
       </div>
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
-        <div className="lg:col-span-2">
-          <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-            <Profile />
-            <CreateEvent />
-          </div>
-          <div className="mt-4">
-            <Charts />
-          </div>
-        </div>
-        <div className="lg:col-span-1">
-          <Events />
-        </div>
-      </div>
     </section>
   );
 };
 
-export default CreatorDashboardPage;
+export default CreatorDashboard;
